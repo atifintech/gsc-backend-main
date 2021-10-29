99bc5c0049c3204dd49f486f45aa753b85a7d7f5
Atif Mahmood <saimtiazm@gmail.com>
2021-10-29 23:18:42 +0600

    Update views.py

--- myapp/myapi/views.py
+++ myapp/myapi/views.py
@@ -27,15 +27,10 @@ from .models import Service
 from .serializers import CustomUserSerializer
 from .models import NewUser
 
-
-
-
-
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
 
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import AllowAny
